b0f4ae62d7023528665b9f7191c98b9b0570e000
Isaac Dalyop <isiquedan@gmail.com>
2020-09-28 19:19:55 +0100

    Checking second module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 
+
 @Injectable()
 export class ProductService {
-  getAlbum() {
-    id: Number;
+  getAlbum(id: number) {
+    return this._http.get(_albumUrl);
   }
 
-  constructor() {
-    private_http: Http;
-    _albumUrl: '../assets/album.json';
+  constructor( private _http: Http) {
+
   }
+  private _albumUrl: ../assets/album.json;
 }
