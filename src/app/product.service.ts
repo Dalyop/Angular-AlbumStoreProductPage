67ba272a8153cc3c3b35a2152ed0a91866b5de40
Isaac Dalyop <isiquedan@gmail.com>
2020-10-03 22:44:34 +0100

    Fifth module checking

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,13 +2,19 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Album } from './album';
+import { Product } from './product';
 import { Observable } from 'rxjs/Observable';
+
 
 
 @Injectable()
 export class ProductService {
 
+  products: Product[];
+
   private _albumUrl = '../assets/album.json';
+
+  private _productsUrl = '../assets/products.json';
 
   constructor( private _http: Http) {
 
@@ -18,5 +24,10 @@ export class ProductService {
     return this._http.get(this._albumUrl).map((response) => <Album>
     response.json());
 
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map((response => <Product>
+      response.json))
   }
 }
