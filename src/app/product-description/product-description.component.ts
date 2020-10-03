4e1f0934b74d5585c000e21d892f333e61cd8e73
Isaac Dalyop <isiquedan@gmail.com>
2020-10-03 15:00:37 +0100

    Second module checking

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -8,7 +8,7 @@ import { ProductService } from '../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  albumInfo;
+  albumInfo: any;
 
   constructor( private _productService: ProductService ) { }
 
